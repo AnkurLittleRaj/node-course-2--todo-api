3aea40ceb990d504e206564cde756179cad68c1d
unknown <raj.ankur8@gmail.com>
2018-08-30 10:20:21 +0530

    updating

--- server/db/moongose.js
+++ server/db/moongose.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
 mongoose.promise = global.promise
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds243325.mlab.com:43325/ankurmongodb'||'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://Little:Ankurjack3@ds243325.mlab.com:43325/ankurmongodb'||'mongodb://localhost:27017/TodoApp');
 module.exports={mongoose}
