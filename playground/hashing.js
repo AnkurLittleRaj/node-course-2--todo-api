d280d4b5e23315d7caf1efc8bb35678769feeab5
unknown <raj.ankur8@gmail.com>
2018-09-06 09:41:32 +0530

    password bcrypted

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,13 +1,18 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
-var data = {
-    id:10
-};
-var token =jwt.sign(data,'123abc');
-console.log(token);
+const bcrypt = require('bcryptjs');
+var password = '123abc!';
+// bcrypt.genSalt(10,(err,salt)=>{
+//     bcrypt.hash(password,salt,(err,hash)=>{
+//         console.log(hash);
+//     })
+// });
 
- var decoded = jwt.verify(token,'123abc');
- console.log(decoded);
+var hashedpwd = '$2a$10$hOCzAjnK3Lhr0RPePU4ZheJCPn0/xDEigF7yUOKNaB0v2/kSCPpRq';
+bcrypt.compare(password,hashedpwd,(err,res)=>{
+    console.log(res);
+})
+//  
 // var message = "Hello i am number 3";
 // var hash = SHA256(message).toString();
 // console.log(`message is ${message}`);
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ var mongoose = require ('mongoose');
 const _ = require('lodash');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 var UserSchema = new mongoose.Schema({
  email :{type : String,
    minlength : 1,
@@ -59,7 +60,21 @@ return User.findOne({
     'tokens.token':token,
     'tokens.access':'auth'
 })
+};
+UserSchema.pre('save',function (next) {
+var user = this;
+if(user.isModified('password')){
+bcrypt.genSalt(10,(err,salt)=>{
+    bcrypt.hash(user.password,salt,(err,hash)=>{
+    user.password = hash;
+    next();
+    })
+});
 }
+else{
+    next();
+}
+})
 var User = mongoose.model('User',UserSchema);
 module.exports = {
     User
