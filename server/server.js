6df7e4c26f284b7f4fdfa094f590c4e215ee33ad
unknown <raj.ankur8@gmail.com>
2018-09-08 01:57:05 +0530

    adding delete/user/token

--- server/server.js
+++ server/server.js
@@ -140,6 +140,13 @@ User.findByCredentials(body.email,body.password).then((user)=>{
 res.status(400).send();
 });
 });
+app.delete('/users/me/token',authenticate,(req,res)=>{
+    req.user.removeToken(req.token).then(()=>{
+res.status(200).send();
+    },()=>{
+  res.status(400).send();      
+    })
+});
 app.listen(port,(() =>{
     console.log("url is working properly on"+port);
 }) )
